b41101cc690ed151cd5f0d9c9f985323299c6058
Aiste Klusaite <aiste.klusaite@gmail.com>
2023-08-15 21:00:39 +0300

    Adding MealItem and MealForm

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+
+import classes from './AvailableMeals.module.scss';
 import { dummyMeals } from "../../assets/Data/DummyMeals";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals: React.FC = () => {
-    const mealsList = dummyMeals.map((meal) => <li>{meal.name}</li>);
+    const mealsList = dummyMeals.map((meal) => 
+        <MealItem
+            mealId={meal.id}
+            key={meal.id} 
+            name={meal.name} 
+            description={meal.description} 
+            price={meal.price} 
+        />);
 
     return (
-        <section>
-            <ul>
-                {mealsList}
-            </ul>
+        <section className={classes.meals}>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 };
